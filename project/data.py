73ed6906f4553568bfa7cb4935c1c666863f35e6
J3FALL <crisp.coad@gmail.com>
2018-05-14 02:19:21 +0300

    Add graph creation

--- project/data.py
+++ project/data.py
@@ -1,6 +1,17 @@
+import networkx as nx
+
+
+def create_graph():
+    users, repos, edges = load_graph()
+    fix_ids(users, repos, edges)
+
+    g = nx.Graph()
+    g.add_edges_from(edges)
+    return g
+
+
 def load_graph():
     file_name = "github/data.txt"
-
     edges = []
     users = {}
     repos = {}
@@ -15,4 +26,18 @@ def load_graph():
     return users, repos, edges
 
 
-load_graph()
+# Add offsets for each repo id to follow the uniqueness of node ids
+def fix_ids(users, repos, edges):
+    offset = len(users.keys())
+
+    idx = 0
+    for repo in repos.keys():
+        repos[repo] = idx + offset
+        idx += 1
+
+    for e in edges:
+        e[1] = repos[e[1]]
+
+
+g = create_graph()
+
